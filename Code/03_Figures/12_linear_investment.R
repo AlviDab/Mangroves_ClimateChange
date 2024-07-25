91cea25ff3c9d92eece6722df71ce6aa11e054c5
Alvise Dabalà <alvise.dab@gmail.com>
2024-07-25 12:53:06 +1000

    Put together global and country scale

--- Code/03_Figures/12_linear_investment.R
+++ Code/03_Figures/12_linear_investment.R
@@ -7,119 +7,145 @@ ncores <- detectCores() - 2
 
 # plan(multisession, workers = ncores)
 
-map(c("landward", "seaward"), function(CC_direction) {
+area_cr <- map(c("landward", "seaward"), function(CC_direction) {
+  map(c("country_and_biotyp",
+        "biotyp"), function(split_group) {
 
-  area_cr <- map(c("country_and_biotyp",
-          "biotyp"), function(split_group) {
+          future_map(seq(0, 1, by = 0.05),
+                     function(prct) {
+                       if(prct == 0) {
+                         solution <- readRDS(paste0("Results/RDS/prioritisation/Country/01_prioritisation/",
+                                                    split_group,"/solution_prioritisation.rds"))
+                       } else {
 
-            future_map(seq(0, 1, by = 0.05),
-                       function(prct) {
-                         if(prct == 0) {
-                           solution <- readRDS(paste0("Results/RDS/prioritisation/Country/01_prioritisation/",
-                                                      split_group,"/solution_prioritisation.rds"))
-                         } else {
+                         solution <- readRDS(paste0("Results/RDS/prioritisation/Country/02_prioritisation_CC/",
+                                                    split_group, "/",
+                                                    CC_direction, "/solution_",
+                                                    as.character(prct), "_", CC_direction, ".rds"))
 
-                           solution <- readRDS(paste0("Results/RDS/prioritisation/Country/02_prioritisation_CC/",
-                                                      split_group, "/",
-                                                      CC_direction, "/solution_",
-                                                      as.character(prct), "_", CC_direction, ".rds"))
+                       }
 
-                         }
+                       area <- solution %>%
+                         st_drop_geometry() %>%
+                         filter(solution_1 == 1) %>%
+                         summarise(Area_mangrove_km2 = sum(MangroveArea_km2))
 
-                         area <- solution %>%
-                           st_drop_geometry() %>%
-                           filter(solution_1 == 1) %>%
-                           summarise(Area_mangrove_km2 = sum(MangroveArea_km2))
+                       #Median resilience
+                       climate_resilient_median_mean <- solution %>%
+                         st_drop_geometry() %>%
+                         filter(solution_1 == 1) %>%
+                         summarise(median_resilience =
+                                     spatstat.univar::weighted.median(Prob_gain_stability_mean,
+                                                                      MangroveArea_km2),
+                                   mean_resilience =
+                                     weighted.mean(Prob_gain_stability_mean,
+                                                   MangroveArea_km2))
+                       # mode_resilience = collapse::fmode(Prob_gain_stability_mean,
+                       #                                   MangroveArea_km2))
 
-                         #Median resilience
-                         climate_resilient_median_mean <- solution %>%
-                           st_drop_geometry() %>%
-                           filter(solution_1 == 1) %>%
-                           summarise(median_resilience =
-                                       spatstat.univar::weighted.median(Prob_gain_stability_mean,
-                                                                        MangroveArea_km2),
-                                     mean_resilience =
-                                       weighted.mean(Prob_gain_stability_mean,
-                                                     MangroveArea_km2))
-                         # mode_resilience = collapse::fmode(Prob_gain_stability_mean,
-                         #                                   MangroveArea_km2))
+                       #Filter mangroves with resilience >= 75
+                       climate_resilient_75 <- solution %>%
+                         st_drop_geometry() %>%
+                         filter(solution_1 == 1,
+                                Prob_gain_stability_mean >= 75) %>%
+                         summarise(area_climate_resilient_75 = sum(MangroveArea_km2))
 
-                         #Filter mangroves with resilience >= 75
-                         climate_resilient_75 <- solution %>%
-                           st_drop_geometry() %>%
-                           filter(solution_1 == 1,
-                                  Prob_gain_stability_mean >= 75) %>%
-                           summarise(area_climate_resilient_75 = sum(MangroveArea_km2))
+                       area_cr <- area %>%
+                         cbind(climate_resilient_median_mean) %>%
+                         cbind(climate_resilient_75) %>%
+                         add_column(threshold = prct)
+                     }) %>%
+            bind_rows() %>%
+            mutate(scale = split_group,
+                   edge = CC_direction) %>%
+            mutate(prct_increase_area = (Area_mangrove_km2 -
+                                           Area_mangrove_km2[1])/Area_mangrove_km2,
+                   prct_increase_mean_resilience = (mean_resilience -
+                                                      mean_resilience[1])/mean_resilience,
+                   ratio_prct_increase = prct_increase_mean_resilience/prct_increase_area*100)
+        })%>%
+    bind_rows()
+}) %>%
+  bind_rows()
 
-                         area_cr <- area %>%
-                           cbind(climate_resilient_median_mean) %>%
-                           cbind(climate_resilient_75) %>%
-                           add_column(threshold = prct)
-                       }) %>%
-              bind_rows() %>%
-              mutate(type = split_group) %>%
-              mutate(prct_increase_area = (Area_mangrove_km2 -
-                                             Area_mangrove_km2[1])/Area_mangrove_km2,
-                     prct_increase_mean_resilience = (mean_resilience -
-                                                        mean_resilience[1])/mean_resilience,
-                     ratio_prct_increase = prct_increase_mean_resilience/prct_increase_area*100)
-          })%>%
-      bind_rows()
+area_cr_global_lw <- area_cr %>%
+  filter(scale == "biotyp",
+         edge == "landward") %>%
+  filter(prct_increase_area != 0)
 
-    area_cr_global <- area_cr %>%
-      filter(type == "biotyp") %>%
-      filter(prct_increase_area != 0)
+area_cr_global_sw <- area_cr %>%
+  filter(scale == "biotyp",
+         edge == "seaward") %>%
+  filter(prct_increase_area != 0)
 
-    area_cr_country <- area_cr %>%
-      filter(type == "country_and_biotyp") %>%
-      filter(prct_increase_area != 0)
+area_cr_country_lw <- area_cr %>%
+  filter(scale == "country_and_biotyp",
+         edge == "landward") %>%
+  filter(prct_increase_area != 0)
 
-    fit_global <- lm(prct_increase_mean_resilience*100 ~ log(prct_increase_area*100),
-                     data = area_cr_global)
-    fit_country <- lm(prct_increase_mean_resilience*100 ~ log(prct_increase_area*100),
-                      data = area_cr_country)
+area_cr_country_sw <- area_cr %>%
+  filter(scale == "country_and_biotyp",
+         edge == "seaward") %>%
+  filter(prct_increase_area != 0)
 
-    scatterplot <- ggplot(data = area_cr, aes(x = prct_increase_area*100,
-                                              y = prct_increase_mean_resilience*100)) +
-      geom_point(aes(colour = type), alpha = 0.7, size = 1) +
-      #geom_line(aes(colour = type), linetype = "dotted", linewidth = 1) +
-      geom_function(fun = function(x) fit_global$coefficients[1] +
-                      fit_global$coefficients[2]*log(x),
-                    colour = '#2a9d8f') +
-      geom_function(fun = function(x) fit_country$coefficients[1] +
-                      fit_country$coefficients[2]*log(x),
-                    colour = '#F4A261') +
-      scale_colour_manual(values = c('#2a9d8f', '#F4A261'),
-                          labels = c('Global scale',
-                                     'Country scale'),
-                          guide = guide_legend()) +
-      theme_classic() +
-      theme(
-        panel.grid.major = element_line(colour = "grey90", size = 0.1),
-        panel.grid.minor = element_line(colour = "grey90", size = 0.05),
-        legend.position = "top"
-      ) +
-      ylab("Percentage increase in resilience (%)") +
-      xlab("Percentage increase in area (%)") +
-      theme(legend.title = element_blank(),
-            axis.title.x = element_text(colour = "grey20"
-                                        #face = "bold"
-                                        ),
-            axis.title.y = element_text(colour = "grey20"
-                                        #face = "bold"
-                                        )) +
-      scale_x_continuous(limits = c(0, NA),
-                         expand = expansion(mult = c(0, 0.1))) +
-      scale_y_continuous(limits = c(0, NA),
-                         expand = expansion(mult = c(0, 0.1)))
+fit_global_lw <- lm(prct_increase_mean_resilience*100 ~ log(prct_increase_area*100),
+                 data = area_cr_global_lw)
+fit_global_sw <- lm(prct_increase_mean_resilience*100 ~ log(prct_increase_area*100),
+                    data = area_cr_global_sw)
+fit_country_lw <- lm(prct_increase_mean_resilience*100 ~ log(prct_increase_area*100),
+                  data = area_cr_country_lw)
+fit_country_sw <- lm(prct_increase_mean_resilience*100 ~ log(prct_increase_area*100),
+                     data = area_cr_country_sw)
 
-    dir.create(paste0("Figures/Country/12_linear_investment/RDS"), recursive = TRUE)
+scatterplot <- ggplot(data = area_cr, aes(x = prct_increase_area*100,
+                                          y = prct_increase_mean_resilience*100)) +
+  geom_point(aes(colour = scale, shape = edge), alpha = 0.7, size = 1) +
+  #geom_line(aes(colour = type), linetype = "dotted", linewidth = 1) +
+  geom_function(fun = function(x) fit_global_lw$coefficients[1] +
+                  fit_global_lw$coefficients[2]*log(x),
+                colour = '#2a9d8f') +
+  geom_function(fun = function(x) fit_global_sw$coefficients[1] +
+                  fit_global_sw$coefficients[2]*log(x),
+                colour = '#2a9d8f',
+                linetype = "dashed") +
+  geom_function(fun = function(x) fit_country_lw$coefficients[1] +
+                  fit_country_lw$coefficients[2]*log(x),
+                colour = '#F4A261') +
+  geom_function(fun = function(x) fit_country_sw$coefficients[1] +
+                  fit_country_sw$coefficients[2]*log(x),
+                colour = '#F4A261',
+                linetype = "dashed") +
+  scale_colour_manual(values = c('#2a9d8f', '#F4A261'),
+                      labels = c('Global scale',
+                                 'Country scale'),
+                      guide = guide_legend()) +
+  scale_shape_manual(values = c(16, 17),
+                     labels = c('Landward',
+                                 'Seaward'),
+                      guide = guide_legend()) +
+  theme_classic() +
+  theme(
+    panel.grid.major = element_line(colour = "grey90", linewidth = 0.1),
+    panel.grid.minor = element_line(colour = "grey90", linewidth = 0.05),
+    legend.position = "top"
+  ) +
+  ylab("Percentage increase in resilience (%)") +
+  xlab("Percentage increase in area (%)") +
+  theme(legend.title = element_blank(),
+        axis.title.x = element_text(colour = "grey20"
+                                    #face = "bold"
+        ),
+        axis.title.y = element_text(colour = "grey20"
+                                    #face = "bold"
+        )) +
+  scale_x_continuous(limits = c(0, NA),
+                     expand = expansion(mult = c(0, 0.1))) +
+  scale_y_continuous(limits = c(0, NA),
+                     expand = expansion(mult = c(0, 0.1)))
 
-    ggsave(plot = scatterplot, paste0("Figures/Country/12_linear_investment/linear_investment_",
-                                      CC_direction, ".pdf"),
-           dpi = 300, width = 18, height = 11, units = "cm")
+dir.create(paste0("Figures/Country/12_linear_investment/RDS"), recursive = TRUE)
 
-    saveRDS(scatterplot, paste0("Figures/Country/12_linear_investment/RDS/map_",
-                                CC_direction, ".rds"))
+ggsave(plot = scatterplot, paste0("Figures/Country/12_linear_investment/linear_investment.pdf"),
+       dpi = 300, width = 18, height = 11, units = "cm")
 
-  })
+saveRDS(scatterplot, paste0("Figures/Country/12_linear_investment/RDS/linear_investment.rds"))
--- Code/03_Figures/13_patchwork_countryscale_investment.R
+++ Code/03_Figures/13_patchwork_countryscale_investment.R
@@ -12,76 +12,66 @@ pacman::p_load(tidyverse, sf, parallel, furrr, purrr, patchwork)
 #            function(prct) {
 prct <- 0.3
 
-map(c("country_and_biotyp",
-      "biotyp"), function(split_group) {
+plot_legend <- readRDS(paste0("Figures/Country/08a_plot_diff_area_resilience/seaward/RDS/diff_area_resilience_seaward_by_country_",
+                              prct, "_legend.rds")) +
+  theme(legend.spacing.y = unit(0, "cm"))
 
-        plot_legend <- readRDS(paste0("Figures/Country/08a_plot_diff_area_resilience/seaward/RDS/diff_area_resilience_seaward_by_country_",
-                                      prct, "_legend.rds")) +
-          theme(legend.spacing.y = unit(0, "cm"))
+plot_lw <- readRDS(paste0("Figures/Country/08a_plot_diff_area_resilience/landward/RDS/diff_area_resilience_landward_by_country_", prct, ".rds"))
 
-        plot_lw <- readRDS(paste0("Figures/Country/08a_plot_diff_area_resilience/landward/RDS/diff_area_resilience_landward_by_country_", prct, ".rds"))
+plot_sw <- readRDS(paste0("Figures/Country/08a_plot_diff_area_resilience/seaward/RDS/diff_area_resilience_seaward_by_country_", prct, ".rds"))
 
-        plot_sw <- readRDS(paste0("Figures/Country/08a_plot_diff_area_resilience/seaward/RDS/diff_area_resilience_seaward_by_country_", prct, ".rds"))
+linear <- readRDS(paste0("Figures/Country/12_linear_investment/RDS/linear_investment.rds"))
 
-        linear_lw <- readRDS(paste0("Figures/Country/12_linear_investment/RDS/map_landward.rds"))
+theme_txt <- theme(title = element_text(size = 11,
+                                        face = 'bold'),
+                   text = element_text(size = 9),
+                   axis.text = element_text(size = 10,
+                                            face = 'bold'))
 
-        linear_sw <- readRDS(paste0("Figures/Country/12_linear_investment/RDS/map_seaward.rds"))
+figure_02_a <- ((plot_lw +
+                   ggtitle("Landward selection") +
+                   theme(legend.position = 'none') +
+                   theme_txt) /
+                  (plot_sw +
+                     ggtitle("Seaward selection") +
+                     theme(legend.position = 'none') +
+                     theme_txt)
+) &
+  theme(plot.tag = element_text(face = 'bold'),
+        title = element_text(size = 11,
+                             face = 'bold'),
+        text = element_text(size = 9),
+        axis.text = element_text(size = 10)) &
+  plot_annotation(tag_levels = list(c('a', 'b')))
 
-        theme_txt <- theme(title = element_text(size = 11,
-                                                face = 'bold'),
-                           text = element_text(size = 9),
-                           axis.text = element_text(size = 10,
-                                                    face = 'bold'))
+figure_02_b <- linear +
+  plot_layout(guides = 'collect',
+              axes = "collect") &
+  theme(plot.tag = element_text(face = 'bold'),
+        title = element_text(size = 10),
+        text = element_text(size = 9),
+        axis.text = element_text(size = 10,
+                                 face = 'plain'),
+        legend.position = 'top',
+        legend.text = element_text(size = 9)) &
+  plot_annotation(tag_levels = list(c('c', 'd')))
 
-        figure_02_a <- ((plot_lw +
-                           ggtitle("Landward selection") +
-                           theme(legend.position = 'none') +
-                           theme_txt) /
-                          (plot_sw +
-                             ggtitle("Seaward selection") +
-                             theme(legend.position = 'none') +
-                             theme_txt)
-                        ) &
-          theme(plot.tag = element_text(face = 'bold'),
-                title = element_text(size = 11,
-                                     face = 'bold'),
-                text = element_text(size = 9),
-                axis.text = element_text(size = 10)) &
-          plot_annotation(tag_levels = list(c('a', 'b')))
+layout <- c(
+  patchwork::area(t = 1, l = 1, b = 30, r = 60),
+  patchwork::area(t = 35, l = 1, b = 185, r = 60),
+  patchwork::area(t = 190, l = 1, b = 250, r = 60)
+)
 
-        figure_02_b <- (linear_lw | linear_sw) +
-          plot_layout(guides = 'collect',
-                      axes = "collect") &
-          theme(plot.tag = element_text(face = 'bold'),
-                title = element_text(size = 10),
-                text = element_text(size = 9),
-                axis.text = element_text(size = 10,
-                                         face = 'plain'),
-                legend.position = 'top',
-                legend.text = element_text(size = 9)) &
-          plot_annotation(tag_levels = list(c('c', 'd')))
+figure_02 <- plot_legend + figure_02_a + figure_02_b +
+  plot_layout(design = layout)
 
-        layout <- c(
-          patchwork::area(t = 1, l = 1, b = 30, r = 60),
-          patchwork::area(t = 35, l = 1, b = 185, r = 60),
-          patchwork::area(t = 190, l = 1, b = 250, r = 60)
-        )
+dir.create("Figures/Country/13_patchwork_countryscale_investment/", recursive = TRUE)
 
-        figure_02 <- plot_legend + figure_02_a + figure_02_b +
-          plot_layout(design = layout)
+ggsave(plot = figure_02, paste0("Figures/Country/13_patchwork_countryscale_investment/patchwork_countryscale_investment_", prct, ".pdf"),
+       dpi = 300, width = 18, height = 27, units = "cm")
 
-        dir.create(paste0("Figures/Country/13_patchwork_countryscale_investment/",
-                          split_group), recursive = TRUE)
-
-        ggsave(plot = figure_02, paste0("Figures/Country/13_patchwork_countryscale_investment/",
-                                        split_group,"/patchwork_countryscale_investment_", prct, ".pdf"),
-               dpi = 300, width = 18, height = 27, units = "cm")
-
-        ggsave(plot = figure_02, paste0("Figures/Country/13_patchwork_countryscale_investment/",
-                                        split_group,"/patchwork_countryscale_investment_", prct, ".jpg"),
-               dpi = 300, width = 18, height = 27, units = "cm")
-      })
-# })
+ggsave(plot = figure_02, paste0("Figures/Country/13_patchwork_countryscale_investment/patchwork_countryscale_investment_", prct, ".jpg"),
+       dpi = 300, width = 18, height = 27, units = "cm")
 
 # plan(sequential)
 
