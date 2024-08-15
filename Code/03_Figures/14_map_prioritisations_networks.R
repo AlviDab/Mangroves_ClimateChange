f63c427103cb8273f677ae9550066b5cb35accd6
Alvise Dabalà <alvise.dab@gmail.com>
2024-08-15 12:26:07 +1000

    Try vertical

--- Code/03_Figures/14_map_prioritisations_networks.R
+++ Code/03_Figures/14_map_prioritisations_networks.R
@@ -4,39 +4,37 @@
 pacman::p_load(tidyverse, sf, patchwork)
 
 nocc_global <- readRDS(paste0("Figures/Country/04_map_large/biotyp/RDS/map_noCC_0.rds")) +
-  labs(tag = "a")
+  labs(tag = "a") +
+  ggtitle("Global-scale climate-naïve")
 
 cc_global <- readRDS(paste0("Figures/Country/04_map_large/biotyp/RDS/map_mean_0.3.rds")) +
-  labs(tag = "b")
+  labs(tag = "b") +
+  ggtitle("Global-scale climate-smart")
 
 nocc_country <- readRDS(paste0("Figures/Country/04_map_large/country_and_biotyp/RDS/map_noCC_0.rds")) +
-  labs(tag = "c")
+  labs(tag = "c") +
+  ggtitle("Country-scale climate-naïve")
 
 cc_country <- readRDS(paste0("Figures/Country/04_map_large/country_and_biotyp/RDS/map_mean_0.3.rds")) +
-  labs(tag = "d")
+  labs(tag = "d") +
+  ggtitle("Country-scale climate-smart")
 
 design <- c(
-  patchwork::area(t = 1, l = 11, b = 10, r = 600),
-  patchwork::area(t = 1, l = 601, b = 10, r = 1190),
-  patchwork::area(t = 11, l = 1, b = 300, r = 10),
-  patchwork::area(t = 301, l = 1, b = 600, r = 10),
-  patchwork::area(t = 11, l = 11, b = 300, r = 600),
-  patchwork::area(t = 11, l = 601, b = 300, r = 1190),
-  patchwork::area(t = 301, l = 11, b = 600, r = 600),
-  patchwork::area(t = 301, l = 601, b = 600, r = 1190)
+"1
+2
+3
+4"
 )
 
-plot_patchwork <- list(ggpubr::text_grob("Climate-naïve", face = "bold"),
-     ggpubr::text_grob("Climate-smart", face = "bold"),
-     ggpubr::text_grob("Global-scale", rot = 90, face = "bold"),
-     ggpubr::text_grob("Country-scale", rot = 90, face = "bold"),
-     nocc_global, cc_global,
+plot_patchwork <- list(nocc_global, cc_global,
      nocc_country, cc_country) %>%
   wrap_plots() +
   plot_layout(guides = 'collect', design = design) &
   theme(legend.position = 'bottom',
         plot.tag = element_text(face = 'bold'),
-        title = element_text(size = 10))
+        title = element_text(size = 10,
+                             face = 'bold'),
+        plot.title = element_text(hjust = 0.5))
 
 dir.create("Figures/Country/14_map_prioritisations_networks/", recursive = TRUE)
 
