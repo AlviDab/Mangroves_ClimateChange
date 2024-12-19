fb768cdca4ed5be7db5131c1f50aeff237fff54d
Alvise Dabalà <alvise.dab@gmail.com>
2024-12-20 10:33:55 +1100

    Updates prioritisation

--- Code/02_prioritisation/01_prioritisation.R
+++ Code/02_prioritisation/01_prioritisation.R
@@ -18,7 +18,7 @@ map(c("country_and_", ""), function(file_name) {
                                 cost_column = "area_km2") %>%
     add_relative_targets(PUs_features_split_targets$targets) %>%
     add_min_set_objective() %>%
-    add_gurobi_solver(gap = 1*10^-5)
+    add_gurobi_solver(gap = 1*10^-4)
 
   solution <- solve(prioritizr_problem)
 
