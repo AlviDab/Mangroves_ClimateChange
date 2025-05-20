4a13026c9d4b0398ebf2aded705afd62a21d3eea
SnBuenafe <tinbuenafe@gmail.com>
2025-05-20 12:15:56 +1000

    Setting up HPC code

--- Code/01_data_processing/00_Development_PUs.R
+++ Code/01_data_processing/00_Development_PUs.R
@@ -2,19 +2,41 @@
 #Date: 18/04/2023
 
 # Updated by Jason Everett (UQ) 14th march 2024
+# Edited by Tin Buenafe 20 May 2025 for HPC functionality
 
 # install.packages("devtools")
-devtools::install_github("https://github.com/MathMarEcol/spatialplanr")
+# devtools::install_github("https://github.com/MathMarEcol/spatialplanr")
 
 # install.packages("devtools")
-devtools::install_github("emlab-ucsb/spatialgridr")
+# devtools::install_github("emlab-ucsb/spatialgridr")
 
 # devtools::install_github("emlab-ucsb/spatialgridr")
-pacman::p_load(sf, tidyverse, spatialgridr, spatialplanr)
+# pacman::p_load(sf, tidyverse, spatialgridr, spatialplanr)
 
+# Load libraries
+library(sf)
+library(tidyverse)
+library(spatialgridr)
+library(spatialplanr)
+
+# Define directories
+args = commandArgs(trailingOnly = TRUE)
+INPUT_DIR = args[1] # 1st argument in the srun Rscript function is the the input directory
+TMP_DIR = Sys.getenv("TMPDIR")
+FIG_DIR = file.path(TMP_DIR, "Figures")
+RESULTS_DIR = file.path(TMPDIR, "Results")
+
+# Use this planning unit area
+PU_AREA = 27000
+PU_AREA_LARGE = 270000
+
+# Define projection
 moll_proj <- "ESRI:54009"
 
-gmw <- sf::st_read("Data/gmw_v3_2020/vector/gmw_v3_2020_vec.shp") %>%
+# Load GMW data
+gmw_data <- sf::st_read(file.path(INPUT_DIR, "vector", "gmw_v3_2020_vec.shp"))
+
+gmw <- gmw_data %>%
   st_transform(moll_proj) %>%
   st_make_valid()
 
@@ -22,7 +44,7 @@ gmw <- sf::st_read("Data/gmw_v3_2020/vector/gmw_v3_2020_vec.shp") %>%
 # Lets try getting the PUs within the bbox of the GMW data
 # then check the intersection of the coverage of the PUs for the actual GMW data.
 
-bb <- sf::st_read("Data/gmw_v3_2020/vector/gmw_v3_2020_vec.shp") %>%
+bb <- gmw_data %>%
   st_bbox()
 bb["ymin"] <- floor(bb["ymin"]) # Round the limits or they won't form a complete boundary
 bb["ymax"] = ceiling(bb["ymax"])
@@ -32,21 +54,21 @@ bndry <- spatialplanr::splnr_get_boundary(bb, res = 1) # Get a boundary
 # Get the PUs for the broader bounding box
 PUs <- spatialgridr::get_grid(bndry, option = "sf_hex",
                               projection_crs = moll_proj,
-                              resolution = 27000) %>%
+                              resolution = PU_AREA) %>%
   sf::st_sf() %>%
   dplyr::mutate(cellID = dplyr::row_number())
 
 # Get the larger PUs for the visualisation
 PUs_large <- spatialgridr::get_grid(bndry, option = "sf_hex",
                               projection_crs = moll_proj,
-                              resolution = 270000) %>%
+                              resolution = PU_AREA_LARGE) %>%
   sf::st_sf() %>%
   dplyr::mutate(cellID = dplyr::row_number())
 
 gg <- ggplot() +
   geom_sf(data = PUs, linewidth = 0.00001)
 
-ggsave("Figures/00_bbox_PUs.pdf", gg)
+ggsave(file.path(FIG_DIR, "00_bbox_PUs.pdf"), gg)
 
 # Now we only want the ones that intersect with
 overlap <- sf::st_intersects(PUs, gmw) %>%
@@ -63,7 +85,7 @@ gg <- ggplot() +
   geom_sf(data = gmw, linewidth = 0.0001, colour = "red", fill = NA) +
   geom_sf(data = PUs, linewidth = 0.0001, fill = NA, colour = "blue")
 
-ggsave("Figures/00_mangrove_PUs.pdf", gg, width = 20, height = 5)
+ggsave(file.path(FIG_DIR, "00_mangrove_PUs.pdf"), gg, width = 20, height = 5)
 
 # Next we can run an intersection to return the actual overlap for each PU to calculate cutoffs
 area <- sf::st_intersection(gmw, PUs) %>%
@@ -77,11 +99,11 @@ PUs <- PUs %>%
   dplyr::mutate(PUArea_km2 = as.numeric(units::set_units(sf::st_area(.), "km2")),
                 MangroveProp = MangroveArea_km2/PUArea_km2)
 
-saveRDS(PUs, file = "Results/RDS/00_PUs_mollweide.rds")
-st_write(PUs, "Results/gpkg/00_PUs_mollweide.gpkg")
+saveRDS(PUs, file = file.path(RESULTS_DIR, "RDS", "00_PUs_mollweide.rds"))
+st_write(PUs, file.path(RESULTS_DIR, "gpkg", "00_PUs_mollweide.gpkg"))
 
-saveRDS(PUs_large, file = "Results/RDS/00_PUs_large_mollweide.rds")
+saveRDS(PUs_large, file = file.path(RESULTS_DIR, "RDS", "00_PUs_large_mollweide.rds"))
 
-rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
-gc() #free up memrory and report the memory usage.
-.rs.restartR()
+# rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
+# gc() #free up memrory and report the memory usage.
+# .rs.restartR()
